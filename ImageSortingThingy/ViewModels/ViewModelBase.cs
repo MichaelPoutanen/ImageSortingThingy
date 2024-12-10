46583cec8db06d433b15b3e2c39037a7010d90ea
Michael Poutanen <michael.poutanen@outlook.de>
2024-12-10 11:04:52 +0100

    Init

---
+++ ImageSortingThingy/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ImageSortingThingy.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ ImageSortingThingy/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ImageSortingThingy.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
